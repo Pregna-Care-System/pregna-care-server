d0a543c72ff6926d9ad28372f3d0ba63c6098651
21anhn <ltnanh21@gmail.com>
2025-01-21 23:32:37 +0700

    [Update] Modify cors policy

--- PregnaCare/Program.cs
+++ PregnaCare/Program.cs
@@ -50,7 +50,7 @@ namespace PregnaCare
                 options.AddPolicy("AllowFrontend",
                     policy =>
                     {
-                        policy.WithOrigins("http://localhost:3000")
+                        policy.AllowAnyOrigin()
                               .AllowAnyHeader()
                               .AllowAnyMethod();
                     });
