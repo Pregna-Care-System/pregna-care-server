70ab3e170227b37905b48f7ac6e04bd28e904a94
21anhn <ltnanh21@gmail.com>
2025-02-10 22:52:52 +0700

    [Update] Remove unused lib

--- PregnaCare/Services/Implementations/TokenService.cs
+++ PregnaCare/Services/Implementations/TokenService.cs
@@ -1,8 +1,6 @@
 ﻿using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
 using System.Text;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.IdentityModel.Tokens;
 using PregnaCare.Common.Enums;
 using PregnaCare.Core.Models;
