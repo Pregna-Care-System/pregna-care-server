aa64147cc03e873daec04bb87dc4027f174adc3d
Pham Tien <phammackimtien@gmail.com>
2025-02-14 00:47:28 +0700

    [UPDATE] Add email for user transaction infor

--- PregnaCare/Core/DTOs/UserMembershipPlanDTO.cs
+++ PregnaCare/Core/DTOs/UserMembershipPlanDTO.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
 
         public Guid UserId { get; set; }
+        public string Email { get; set; }
 
         public string FullName { get; set; }
         public Guid MembershipPlanId { get; set; }
