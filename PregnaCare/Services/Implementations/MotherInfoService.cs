26ae09789ebbeae5f06ac2802ac53fe42fd0799d
21anhn <ltnanh21@gmail.com>
2025-02-18 23:19:50 +0700

    [Add] Add API MotherInfo

---
+++ PregnaCare/Services/Implementations/MotherInfoService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PregnaCare.Core.Models;
+using PregnaCare.Infrastructure.Data;
+using PregnaCare.Services.Interfaces;
+
+namespace PregnaCare.Services.Implementations
+{
+    public class MotherInfoService : IMotherInfoService
+    {
+        private readonly PregnaCareAppDbContext _context;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="context"></param>
+        public MotherInfoService(PregnaCareAppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<MotherInfo> GetAllMotherInfosByUserId(Guid userId)
+        {
+            return _context.MotherInfos.AsNoTracking().Include(x => x.PregnancyRecord)
+                                               .Include(x => x.PregnancyRecord.User)
+                                               .Where(x => x.PregnancyRecord.UserId == userId);
+        }
+    }
+}
---
+++ PregnaCare/Services/Interfaces/IMotherInfoService.cs
@@ -0,0 +1,9 @@
+﻿using PregnaCare.Core.Models;
+
+namespace PregnaCare.Services.Interfaces
+{
+    public interface IMotherInfoService
+    {
+        public IEnumerable<MotherInfo> GetAllMotherInfosByUserId(Guid userId);
+    }
+}
