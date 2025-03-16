81d4f975f1affc81d73227ce39bf7de3ba86277d
21anhn <ltnanh21@gmail.com>
2025-03-16 19:14:34 +0700

    [Add] Add function GetTimeAgo

--- PregnaCare/Utils/CommonUtils.cs
+++ PregnaCare/Utils/CommonUtils.cs
@@ -14,5 +14,26 @@ namespace PregnaCare.Utils
                 return otp.ToString("D6");
             }
         }
+
+        public static string GetTimeAgo(DateTime dateTime)
+        {
+            var timeSpan = DateTime.UtcNow.Subtract(dateTime);
+
+            if (timeSpan.TotalSeconds < 60)
+                return "just now";
+            if (timeSpan.TotalMinutes < 60)
+                return $"{(int)timeSpan.TotalMinutes} {(timeSpan.TotalMinutes == 1 ? "minute" : "minutes")} ago";
+            if (timeSpan.TotalHours < 24)
+                return $"{(int)timeSpan.TotalHours} {(timeSpan.TotalHours == 1 ? "hour" : "hours")} ago";
+            if (timeSpan.TotalDays < 7)
+                return $"{(int)timeSpan.TotalDays} {(timeSpan.TotalDays == 1 ? "day" : "days")} ago";
+            if (timeSpan.TotalDays < 30)
+                return $"{(int)(timeSpan.TotalDays / 7)} {((int)(timeSpan.TotalDays / 7) == 1 ? "week" : "weeks")} ago";
+            if (timeSpan.TotalDays < 365)
+                return $"{(int)(timeSpan.TotalDays / 30)} {((int)(timeSpan.TotalDays / 30) == 1 ? "month" : "months")} ago";
+
+            return $"{(int)(timeSpan.TotalDays / 365)} {((int)(timeSpan.TotalDays / 365) == 1 ? "year" : "years")} ago";
+        }
+
     }
 }
