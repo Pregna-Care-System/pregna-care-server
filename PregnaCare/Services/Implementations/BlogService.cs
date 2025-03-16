bd4e48ee6bb8d2197db9223393f81effb701ad02
21anhn <ltnanh21@gmail.com>
2025-03-16 23:29:10 +0700

    [Update] Add field Feature

--- PregnaCare/Services/Implementations/BlogService.cs
+++ PregnaCare/Services/Implementations/BlogService.cs
@@ -106,7 +106,6 @@ namespace PregnaCare.Services.Implementations
             blog.PageTitle = request.PageTitle;
             blog.UpdatedAt = DateTime.Now;
             blog.Type = request.Type;
-            blog.Status = request.Status;
             blog.SharedChartData = request.SharedChartData;
             blog.IsVisible = request.IsVisible;
 
